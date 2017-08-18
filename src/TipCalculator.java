3beeb2b9c4bf3c0943cf746b0727a259f684930f
Kostia Sokolovskii <drozdon@rambler.ru>
2017-08-18 15:54:44 +0300

    Create TipCalculator

---
+++ src/TipCalculator.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by falc0n on 18.08.2017.
+ */
+public class TipCalculator extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+
+        Scene scene = new Scene(root); // attach scene graph to scene
+        stage.setTitle("TipCalculator"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
+
+    }
+
+    public static void main(String[] args) {
+        // create a TipCalculator object and call its start method
+        launch(args);
+    }
+
+}
